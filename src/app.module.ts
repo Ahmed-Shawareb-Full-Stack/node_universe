d7d58950ad1a82a05153dd4310eeaa7c9fb300e9
Ahmed Shawareb <js.dev.ahmedshawareb@gmail.com>
2023-11-14 14:52:37 +0200

    setup main config 'database'

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,34 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: 'postgres',
+          host: configService.get<string>('DB_HOST'),
+          port: +configService.get('DB_PORT'),
+          database: configService.get<string>('DB_NAME'),
+          username: configService.get<string>('DB_USERNAME'),
+          password: configService.get<string>('DB_PASSWORD'),
+          entities: [],
+          synchronize:
+            configService.get<string>('DB_PASSWORD') === 'development'
+              ? true
+              : false,
+        };
+      },
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
